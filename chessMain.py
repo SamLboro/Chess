f9f6f0fa47c2fdf8c12242057a2bdf0940034eb6
SamLboro <84963505+SamLboro@users.noreply.github.com>
2022-09-10 14:21:33 +0100

    Update chessMain.py

--- chessMain.py
+++ chessMain.py
@@ -1,3 +1,25 @@
 """
 This is our main driver file. It will be responsible for handling user input and displaying the current GameState object. 
 """
+
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512 #400 is another option
+DIMENSION = 8 #dimensions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 #for animations later on
+IMAGES = {}
+
+'''
+Initialise a global dictionary of images. This will be called exactly once in the main. 
+'''
+
+def loadImages():
+  pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bN", "bR", "bB", "bQ", "bK"]
+  for piece in pieces:
+    IMAGES[piece] = p.image.load("images/" + piece + ".png")
+  #Note: we can access an image by saying 'IMAGES["wP"]'
+  
+  
+  
