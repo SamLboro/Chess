df90b94293291a55d01a7dcc1c09dafe4b52fc8c
SamLboro <84963505+SamLboro@users.noreply.github.com>
2022-09-10 14:12:51 +0100

    Update chessEngine.py

--- chessEngine.py
+++ chessEngine.py
@@ -18,5 +18,9 @@ class GameState():
       ["--", "--", "--", "--", "--", "--", "--", "--"],
       ["--", "--", "--", "--", "--", "--", "--", "--"],
       ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+    ]
+    
+    self.whiteToMove = True
+    self.moveLog = []
     
