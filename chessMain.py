a8864d75f449f0b8f48ea7357a7972a5a1a12e8c
SamLboro <84963505+SamLboro@users.noreply.github.com>
2022-09-10 14:24:03 +0100

    Update chessMain.py

--- chessMain.py
+++ chessMain.py
@@ -5,6 +5,7 @@ This is our main driver file. It will be responsible for handling user input and
 import pygame as p
 from Chess import ChessEngine
 
+p.init()
 WIDTH = HEIGHT = 512 #400 is another option
 DIMENSION = 8 #dimensions of a chess board are 8x8
 SQ_SIZE = HEIGHT // DIMENSION
@@ -18,8 +19,12 @@ Initialise a global dictionary of images. This will be called exactly once in th
 def loadImages():
   pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bN", "bR", "bB", "bQ", "bK"]
   for piece in pieces:
-    IMAGES[piece] = p.image.load("images/" + piece + ".png")
+    IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
   #Note: we can access an image by saying 'IMAGES["wP"]'
-  
-  
+
+'''
+The main driver for our code. This will handle user input and updating the graphics
+'''
+
+def main():
   
