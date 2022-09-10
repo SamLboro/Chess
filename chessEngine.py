3290cc6b989b6ef50af4052f46870bf8f450f110
SamLboro <84963505+SamLboro@users.noreply.github.com>
2022-09-10 14:11:17 +0100

    Update chessEngine.py

--- chessEngine.py
+++ chessEngine.py
@@ -1,3 +1,22 @@
 """
-This class is responsible for storing all the information about the current state of a chess game/
+This class is responsible for storing all the information about the current state of a chess game. It will also be responsible for determining the valid moves at the
+current state. It will also keep a move log.
 """
+
+class GameState():
+  def __init__(self):
+    #The board is 8x8 2d list, each element of the list has 2 characters. 
+    #The first character represents the colour of the piece, 'b' or 'w'
+    #The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N', 'p'
+    #The string "--" represents an empty space with no piece.
+    
+    self.board = [
+      ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+      ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+      ["--", "--", "--", "--", "--", "--", "--", "--"],
+      ["--", "--", "--", "--", "--", "--", "--", "--"],
+      ["--", "--", "--", "--", "--", "--", "--", "--"],
+      ["--", "--", "--", "--", "--", "--", "--", "--"],
+      ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+      ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+    
