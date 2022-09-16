aa36b1340796afd97fa96dd69072c18c380499f9
SamLboro <84963505+SamLboro@users.noreply.github.com>
2022-09-16 12:41:18 +0100

    Update chessEngine.py

--- chessEngine.py
+++ chessEngine.py
@@ -17,10 +17,13 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "bp", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
         ]
+        self.moveFunctions = {"p": self.getPawnMoves, "R": self.getRookMoves, "N": self.getKnightMoves,
+                              "B": self.getBishopMoves, "Q": self.getQueenMoves, "K": self.getKingMoves
+        }
 
         self.whiteToMove = True
         self.moveLog = []
@@ -54,34 +57,62 @@ class GameState():
     All moves without considering checks
     '''
     def getAllPossibleMoves(self):
-        moves = [Move((6, 4), (4, 4), self.board)]
+        moves = []
         for r in range(len(self.board)): #The number of rows
             for c in range(len(self.board[r])): #The number of columns in given row
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) and (turn == "b" and not self.whiteToMove):
+                if (turn == 'w' and self.whiteToMove) or (turn == "b" and not self.whiteToMove):
                     piece = self.board[r][c][1]
-                    if piece == "p":
-                        self.getPawnMoves(r, c, moves)
-                    elif piece == "R":
-                        self.getRookMoves(r, c, moves)
-
+                    self.moveFunctions[piece](r, c, moves) #Calls the appropriate move function based on piece type
         return moves
 
     '''
     This will get all the pawn moves for the pawn located at row, col and add these moves to the list
     '''
-    def getPawnMoves(r, c, moves):
-        pass
+    def getPawnMoves(self, r, c, moves):
+        if self.whiteToMove: #White pawn moves
+            if self.board[r-1][c] == "--":
+                moves.append(Move((r, c), (r-1, c), self.board))
+                if r == 6 and self.board[r-2][c] == "--": #2 square pawn advance
+                    moves.append(Move((r, c), (r-2, c), self.board))
+            if c-1 >= 0: #Captures to the left
+                if self.board[r-1][c-1][0] == "b": #Enemy piece to capture
+                    moves.append(Move((r, c), (r - 1, c-1), self.board))
+            if c+1 <= 7: #Captures to the right
+                if self.board[r-1][c+1][0] == "b":
+                    moves.append(Move((r, c), (r - 1, c + 1), self.board))
+
+        return moves
 
     '''
     This will get all the rook moves for the rook located at row, col and add these moves to the list
     '''
-    def getRookMoves(r, c, moves):
+    def getRookMoves(self, r, c, moves):
         pass
 
+    '''
+    This will get all the knight moves for the rook located at row, col and add these moves to the list
+    '''
+    def getKnightMoves(self, r, c, moves):
+        pass
 
+    '''
+    This will get all the bishop moves for the rook located at row, col and add these moves to the list
+    '''
+    def getBishopMoves(self, r, c, moves):
+        pass
 
+    '''
+    This will get all the king moves for the rook located at row, col and add these moves to the list
+    '''
+    def getKingMoves(self, r, c, moves):
+        pass
 
+    '''
+    This will get all the queen moves for the rook located at row, col and add these moves to the list
+    '''
+    def getQueenMoves(self, r, c, moves):
+        pass
 
 class Move():
     #This maps keys to values
@@ -101,7 +132,6 @@ class Move():
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
         self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
-        print(self.moveID)
 
     '''
     Overriding the equals method
@@ -117,4 +147,3 @@ class Move():
 
     def getRankFile(self, r, c):
         return self.colsToFiles[c] + self.rowsToRanks[r]
-
